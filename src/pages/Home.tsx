20efaf79cc06729c8b6f22e71255a65f3952d8b8
nellfs <pokycelit@gmail.com>
2023-02-03 23:00:54 -0300

    setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="container">
+        <div className="title">
+          <h1>
+            Meu nome é <b>Heron</b>
+          </h1>
+          <div className="line"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
